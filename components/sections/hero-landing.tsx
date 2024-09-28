c303a9f867903624a09434c5925ed9a78352003f
jcarpio <jose.carpio@gmail.com>
2024-09-28 22:53:20 +0200

    Update hero-landing.tsx

--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -27,7 +27,7 @@ export default async function HeroLanding() {
         </Link>
 
         <h1 className="text-balance font-urban text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-[66px]">
-          DreamBez{" "}
+          . {" "}
           <span className="text-gradient_indigo-purple font-extrabold">
             DreamBez.com
           </span>
