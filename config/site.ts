29bc027612993c09e55a38bb372723cc2fd6f581
jcarpio <jose.carpio@gmail.com>
2024-09-28 23:18:40 +0200

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -4,23 +4,23 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  title: "Fun & Pro AI Headshots Generator | HeadShots.fun",
-  name: "HeadShots.fun",
+  title: "Regala Sueños | DreamBez.com",
+  name: "DreamBez.com",
   description:
-    "Create stunning fun and professional headshots with AI at HeadShots.fun. Fast, affordable, and personalized AI-powered headshot generator. Perfect for LinkedIn, resumes, and social media.",
+    "En DreamBez.com creamos tu Álbum de Sueños Hiper Realista con Inteligencia Artificial y mucho Amor.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
-    twitter: "https://twitter.com/ullr_ai",
-    github: "https://github.com/ullrai/HeadShots.fun",
+    twitter: "https://twitter.com/jcarpio",
+    github: "https://github.com/jcarpio/DreamBez",
   },
-  mailSupport: "support@headshots.fun",
+  mailSupport: "soporte@dreambez.com",
   structuredData: {
     "@context": "https://schema.org",
     "@type": "WebSite",
-    "name": "HeadShots.fun",
+    "name": "DreamBez.com",
     "url": site_url,
-    "description": "Create stunning fun and professional headshots with AI at HeadShots.fun. Fast, affordable, and personalized AI-powered headshot generator. Perfect for LinkedIn, resumes, and social media.",
+    "description": "En DreamBez.com creamos tu Álbum de Sueños Hiper Realista con Inteligencia Artificial y mucho Amor.",
     "potentialAction": {
       "@type": "SearchAction",
       "target": `${site_url}/search?q={search_term_string}`,
@@ -28,15 +28,15 @@ export const siteConfig: SiteConfig = {
     }
   },
   keywords: [
-      "Headshots",
+      "Dreams",
   ],
   authors: [
     {
-      name: "UllrAI",
+      name: "Enkire",
     },
   ],
-  creator: "UllrAI",
-  twitterCreator: "@UllrAI",
+  creator: "Enkire",
+  twitterCreator: "@enkire",
   icons: "/favicon.ico",
   manifest: `${site_url}/site.webmanifest`,
 };
@@ -51,11 +51,10 @@ export const footerLinks: SidebarNavItem[] = [
     ],
   },
   {
-    title: "Ullr AI Products",
+    title: "Enkire Products",
     items: [
-      { title: "PixMiller", href: "https://pixmiller.com" },
-      { title: "HeadShots.fun", href: "https://headshots.fun" },
-      { title: "Ullr AI Lab", href: "https://ullrai.com" },
+      { title: "DreamBez", href: "https://DreamBez.com" },
+      { title: "Enkire", href: "https://enkire.com" },
     ],
   },
   {
