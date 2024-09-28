81f98d9b9a469dbf732a546f2ecba2574f09d5be
jcarpio <jose.carpio@gmail.com>
2024-09-28 23:22:40 +0200

    Update preview-landing.tsx

--- components/sections/preview-landing.tsx
+++ components/sections/preview-landing.tsx
@@ -8,7 +8,7 @@ export default function PreviewLanding() {
       <MaxWidthWrapper>
         <div className="rounded-xl md:bg-muted/30 md:p-3.5 md:ring-1 md:ring-inset md:ring-border">
           <div className="relative aspect-video overflow-hidden rounded-xl border md:rounded-lg">
-            <!-- a href="/generator-styles">
+            <a href="/generator-styles">
               <Image
                 className="object-cen size-full object-cover dark:opacity-75"
                 src="/_static/index/preview-landing.png"
@@ -17,7 +17,7 @@ export default function PreviewLanding() {
                 height={840}
                 priority={true}
               />
-            </a -->
+            </a>
           </div>
         </div>
       </MaxWidthWrapper>
