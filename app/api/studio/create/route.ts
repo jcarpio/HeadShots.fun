0cfeb09743c9ec3984c71f15f11273bedd785dea
jcarpio <jose.carpio@gmail.com>
2024-10-01 20:15:42 +0200

    Update route.ts

--- app/api/studio/create/route.ts
+++ app/api/studio/create/route.ts
@@ -7,7 +7,7 @@ export const POST = auth(async (req) => {
   }
 
   try {
-    const { name, type, modelUser, modelVersion, hf_lora, images } = await req.json();
+    const { name, type, modelUser, modelVersion, hf_lora, defaultHairStyle, images } = await req.json();
 
     if (!name || !type || !modelUser || !modelVersion || !hf_lora || !defaultHairStyle || !images || images.length === 0) {
       return new Response("Missing required fields", { status: 400 });
