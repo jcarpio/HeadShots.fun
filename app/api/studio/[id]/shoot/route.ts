db3ae58943d3a7e0ba0d27db34a940bbcb17397f
jcarpio <jose.carpio@gmail.com>
2024-10-01 15:10:16 +0200

    Update route.ts

--- app/api/studio/[id]/shoot/route.ts
+++ app/api/studio/[id]/shoot/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request, { params }: { params: { id: string 
         }
         
         const default_negative_prompt = "flaws in the eyes, flaws in the face, flaws, lowres, non-HDRi, low quality, worst quality,artifacts noise, text, watermark, glitch, deformed, mutated, ugly, disfigured, hands, low resolution, partially rendered objects,  deformed or partially rendered eyes, deformed, deformed eyeballs, cross-eyed,blurry,border, picture frame";
-        const final_prompt = prompt.replace(`{prompt}`, `a ${studio.type} `) + `,headshot of a ${studio.type} `;
+        const final_prompt = prompt.replace(`{prompt}`, `a ${studio.type} `) + `,headshot of joselapasion, a ${studio.type} `;
         console.log("final_prompt", final_prompt);
 
         // Set width and height based on aspectRatio
