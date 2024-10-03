d032c84b85a5069a6edc985b04baa86501a4cb23
jcarpio <jose.carpio@gmail.com>
2024-10-04 00:53:42 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -30,6 +30,7 @@ export async function createCheckoutSession(
         quantity: 1,
       },
     ],
+    allow_promotion_codes: true,
     mode: 'payment',
     success_url: `${env.NEXT_PUBLIC_APP_URL}/payment-status?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${env.NEXT_PUBLIC_APP_URL}/pricing`,
