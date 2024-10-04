aaece970a0a6b15569578dacfd308de949e2fadf
jcarpio <jose.carpio@gmail.com>
2024-10-04 15:40:40 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -50,35 +50,43 @@ export async function handleSuccessfulSubscriptionPayment(invoice: Stripe.Invoic
 
 // Function to create a Stripe subscription checkout session
 export async function createCheckoutSessionForSubscription(
-  priceId: string, // The price ID for the monthly subscription plan
+  priceId: string,
   userId: string,
   emailAddress: string
 ) {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price: priceId, // Use the predefined price ID from Stripe for subscriptions
-        quantity: 1,
+  try {
+    console.log('Creating session with priceId:', priceId);
+    
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: priceId, // Use the predefined price ID from Stripe for subscriptions
+          quantity: 1,
+        },
+      ],
+      billing_address_collection: "auto",
+      customer_creation: "if_required",
+      subscription_data: {
+        metadata: {
+          userId, // Store user ID in metadata
+        },
       },
-    ],
-    billing_address_collection: "auto",
-    customer_creation: "if_required",
-    subscription_data: {
-      metadata: {
-        userId, // Store user ID in metadata
+      allow_promotion_codes: true,
+      automatic_tax: {
+        enabled: true,
       },
-    },
-    allow_promotion_codes: true,
-    automatic_tax: {
-      enabled: true,
-    },
-    mode: "subscription", // Switch to subscription mode
-    success_url: `${env.NEXT_PUBLIC_APP_URL}/payment-status?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${env.NEXT_PUBLIC_APP_URL}/pricing`,
-    customer_email: emailAddress, // Prepopulate the customer email
-  });
+      mode: "subscription",
+      success_url: `${env.NEXT_PUBLIC_APP_URL}/payment-status?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${env.NEXT_PUBLIC_APP_URL}/pricing`,
+      customer_email: emailAddress, // Prepopulate the customer email
+    });
 
-  return session; // Return the complete Stripe session
+    console.log('Session created:', session);
+    return session;
+
+  } catch (error) {
+    console.error('Error creating Stripe session:', error);
+    throw new Error('Failed to generate user stripe session');
+  }
 }
-
