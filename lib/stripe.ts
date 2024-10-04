30ac7fe3a3ff9bd91319918b8c99c6823d2d509a
jcarpio <jose.carpio@gmail.com>
2024-10-04 17:25:46 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,27 +1,22 @@
-// /lib/stripe.ts
-
 import Stripe from "stripe";
 import { env } from "@/env.mjs";
 
-// Create a Stripe instance using the secret key from the environment variables
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: "2024-04-10",
   typescript: true,
 });
 
-// Function to create a Stripe checkout session for subscriptions
 export async function createCheckoutSessionForSubscription(
-  priceId: string, // The Stripe price ID for the subscription plan
+  priceId: string,
   userId: string,
   emailAddress: string
 ) {
   try {
-    // Create the checkout session
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: [
         {
-          price: priceId, // The price ID from Stripe
+          price: priceId,
           quantity: 1,
         },
       ],
@@ -29,23 +24,22 @@ export async function createCheckoutSessionForSubscription(
       customer_creation: "if_required",
       subscription_data: {
         metadata: {
-          userId, // Store user ID in metadata
+          userId,
         },
       },
       allow_promotion_codes: true,
       automatic_tax: {
         enabled: true,
       },
-      mode: "subscription", // Use subscription mode
+      mode: "subscription",
       success_url: `${env.NEXT_PUBLIC_APP_URL}/payment-status?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${env.NEXT_PUBLIC_APP_URL}/pricing`,
-      customer_email: emailAddress, // Prepopulate the customer email
+      customer_email: emailAddress,
     });
 
-    return session; // Return the created session
+    return session;
   } catch (error) {
     console.error("Error creating Stripe session:", error);
     throw new Error("Failed to generate user stripe session");
   }
 }
-
