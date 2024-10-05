943e2c534cb3b7b3b6c1044b4c0ef054de8aef45
jcarpio <jose.carpio@gmail.com>
2024-10-05 17:55:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { withContentlayer } = require("next-contentlayer2");
 import("./env.mjs");
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -32,24 +33,27 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@prisma/client"],
   },
 
-    // Add redirects to handle invitation-based access
   async redirects() {
     return [
       {
         source: '/', // The home page
         destination: '/waitlist', // Redirect to the waitlist page
-        permanent: true,
+        permanent: false, // Set it to false to make it a temporary redirect
         has: [
           {
             type: 'query',
-            key: 'code', // Check for the invitation code in the query parameter
-            value: '(?!valid)', // Regex to ensure the code is not "valid"
+            key: 'code', // If no 'code' query parameter, redirect to /waitlist
+            value: '(?!valid)', // This will allow "valid" codes to bypass the waitlist
           },
         ],
       },
+      {
+        source: '/waitlist',
+        destination: '/waitlist', // Make sure /waitlist is visible directly
+        permanent: false,
+      },
     ];
   },
-  
 };
 
 module.exports = withContentlayer(nextConfig);
