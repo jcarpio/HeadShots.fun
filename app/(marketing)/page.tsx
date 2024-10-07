3909c6213de2bd565848d30fb2a047d6e65a066e
jcarpio <jose.carpio@gmail.com>
2024-10-07 23:20:23 +0200

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { categories, styles, domainPath } from '@/components/shared/styles';
