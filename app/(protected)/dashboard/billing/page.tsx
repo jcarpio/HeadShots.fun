125d90511b94c66e553ab6602c0c52f416304865
jcarpio <jose.carpio@gmail.com>
2024-10-07 16:07:44 +0200

    Update page.tsx

--- app/(protected)/dashboard/billing/page.tsx
+++ app/(protected)/dashboard/billing/page.tsx
@@ -4,6 +4,7 @@ import { constructMetadata } from "@/lib/utils";
 import { DashboardHeader } from "@/components/dashboard/header";
 import { siteConfig } from "@/config/site";
 import { CreditTransactionHistory } from "@/components/dashboard/billing-credits-list";
+import { CustomerPortalButton } from "@/components/forms/customer-portal-button";
 
 export const metadata = constructMetadata({
   title: `Billing – ${siteConfig.title}`,
@@ -12,7 +13,17 @@ export const metadata = constructMetadata({
 
 export default async function BillingPage() {
   const user = await getCurrentUser();
+  const stripeCustomerId = await getStripeCustomerId();
 
+   {isPaid && stripeCustomerId ? (
+          <CustomerPortalButton userStripeId={stripeCustomerId} />
+        ) : (
+          <Link href="/pricing" className={cn(buttonVariants())}>
+            Choose a plan
+          </Link>
+        )}
+
+  
   return (
     <>
       <DashboardHeader
@@ -23,7 +34,7 @@ export default async function BillingPage() {
         <CreditTransactionHistory />
       </div>
       <div className="my-8 text-center text-sm text-muted-foreground">
-        <p>If you have any questions about billing, please contact us at <a href="mailto:support@headshots.fun">support@headshots.fun</a></p>
+        <p>If you have any questions about billing, please contact us at <a href="mailto:support@dreambez.com">support@headshots.fun</a></p>
       </div>
     </>
   );
