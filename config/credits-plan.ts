52922fc06ee88f9c2afaf0329c5ffad4c3907a59
jcarpio <jose.carpio@gmail.com>
2024-10-08 22:05:22 +0200

    Update credits-plan.ts

--- config/credits-plan.ts
+++ config/credits-plan.ts
@@ -3,43 +3,27 @@ import { env } from "@/env.mjs";
 export const pricingData = [
 
   {
-    title: "Starter",
-    priceId: process.env.NEXT_PUBLIC_STRIPE_TESTER_CREDITS_PRICE_ID ?? "",  // Tomado de la variable de entorno
-    description: "Casual Charmer: Spice up your social media game",
+    title: "Photos PLUS",
     price: 9.99,
-    quantity: 40, // credits amount
-    features: ["40 Credits"],
-  },
-/*
-  {
-    price: 0.99,
-    description: "Tester: Only to to see my firsts shots",
-    features: ["3 Credits"],
-    quantity: 3,
+    priceId: process.env.NEXT_PUBLIC_STRIPE_PLUS_CREDITS_PRICE_ID ?? "", 
+    description: "",
+    features: ["PLUS 20 Hyper Realistic photos"],
+    quantity: 20, // credits amount
   },
   {
-    price: 9.99,
-    description: "Casual Charmer: Spice up your social media game",
-    features: ["40 Credits"],
-    quantity: 40,
-  },
-  {
-    price: 19.99,
-    description: "Portrait Pro: Elevate your online presence",
-    features: ["100 Credits"],
-    quantity: 100,
-  },
-  {
+    title: "Starter Pack",
     price: 29.99,
-    description: "Team Transformer: Make your whole squad shine",
-    features: ["200 Credits"],
-    quantity: 200,
+    priceId: process.env.NEXT_PUBLIC_STRIPE_STARTER_CREDITS_PRICE_ID ?? "", 
+    description: "",
+    features: ["10% Discount, 1 Hyper Realistic Model + 40 photos"],
+    quantity: 65,
   },
   {
-    price: 39.99,
-    description: "Team Transformer: Make your whole squad shine",
-    features: ["400 Credits"],
-    quantity: 400,
+    title: "Pro Pack",
+    price: 69.99,
+    priceId: process.env.NEXT_PUBLIC_STRIPE_PRO_CREDITS_PRICE_ID ?? "", 
+    description: "",
+    features: ["20% Discount, 3 Hyper Realistic Models + 94 photos"],
+    quantity: 169,
   },
-  */
 ];
