e0460e40c10e6febe55dcb3d6562b67d40a90ee8
jcarpio <jose.carpio@gmail.com>
2024-10-09 00:59:14 +0200

    Update credits-plan.ts

--- config/credits-plan.ts
+++ config/credits-plan.ts
@@ -5,16 +5,16 @@ export const pricingData = [
   {
     title: "Photos PLUS",
     price: 9.99,
-    regularPrice: 9.99,
+    regularPrice: 10.50,
     priceId: process.env.NEXT_PUBLIC_STRIPE_PLUS_CREDITS_PRICE_ID ?? "", 
     description: "",
-    features: ["20 Hyper Realistic photos"],
+    features: ["5% Discount, 20 Hyper Realistic photos"],
     quantity: 20, // credits amount
   },
   {
     title: "Starter Pack",
     price: 29.99,
-    regularPrice: 33.33, 
+    regularPrice: 33.00, 
     priceId: process.env.NEXT_PUBLIC_STRIPE_STARTER_CREDITS_PRICE_ID ?? "", 
     description: "",
     features: ["10% Discount, 1 Hyper Realistic Model + 40 Hyper Realistic photos"],
@@ -23,7 +23,7 @@ export const pricingData = [
   {
     title: "Pro Pack",
     price: 69.99,
-    regularPrice: 87.49,
+    regularPrice: 87,
     priceId: process.env.NEXT_PUBLIC_STRIPE_PRO_CREDITS_PRICE_ID ?? "", 
     description: "",
     features: ["20% Discount, 3 Hyper Realistic Models + 94 Hyper Realistic photos"],
