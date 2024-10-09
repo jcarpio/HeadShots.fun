558912fbb216e27554d6adaa44ce579b5ed951ef
jcarpio <jose.carpio@gmail.com>
2024-10-09 09:20:09 +0200

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -4,23 +4,23 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  title: "Regala Sueños | DreamBez.com",
+  title: "Gift Dreams | DreamBez.com",
   name: "DreamBez.com",
   description:
-    "En DreamBez.com creamos tu Álbum de Sueños Hiper Realista con Inteligencia Artificial y mucho Amor.",
+    "At DreamBez.com, we create your Hyper Realistic Dream Album with Artificial Intelligence and lots of Love.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
     twitter: "https://twitter.com/jcarpio",
     github: "https://github.com/jcarpio/DreamBez",
   },
-  mailSupport: "soporte@dreambez.com",
+  mailSupport: "support@enkire.com",
   structuredData: {
     "@context": "https://schema.org",
     "@type": "WebSite",
     "name": "DreamBez.com",
     "url": site_url,
-    "description": "En DreamBez.com creamos tu Álbum de Sueños Hiper Realista con Inteligencia Artificial y mucho Amor.",
+    "description": "At DreamBez.com, we create your Hyper Realistic Dream Photos and Album with Artificial Intelligence and lots of Love.",
     "potentialAction": {
       "@type": "SearchAction",
       "target": `${site_url}/search?q={search_term_string}`,
